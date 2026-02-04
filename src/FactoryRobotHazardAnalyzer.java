1caea095a829016e49500ad634828c6b432f7b70
BL-Balaji <balaji.sapkal@bridgelabz.com>
2026-02-04 15:49:18 +0530

    [Balaji] Added : UC1: Display Static Hazard Message.

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,17 @@
+/*
+ * UC1: Display Static Hazard Message
+ *
+ * Purpose:
+ * - This is the entry point of the application.
+ * - It displays a static message indicating the purpose of the system.
+ *
+ * Concepts Used:
+ * - Class declaration
+ * - main() method
+ * - Print statement
+ */
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
